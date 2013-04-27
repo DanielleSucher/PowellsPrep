ed63030a44286d9e0fd298ba9d9f611d32742a4a
Danielle Sucher <dsucher@gmail.com>
2013-04-27 01:50:11 -0400

    Book::mark_all_cheaper_at_powells

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,24 @@
 class Book
-  attr_accessor :_id, :amazon_total_cost, :attributes, :author, :isbn, :locations, :title
+  attr_accessor :_id, :amazon_total_cost, :attributes, :author, :cheaper_at_powells, :isbn, :locations, :title
 
   def self.all
     database.all(:books).map { |book| Book.new book }
+  end
+
+  def self.create(attributes)
+    book = new(attributes)
+    book.save
   end
 
   def self.find(isbn)
     database.find_book isbn
   end
 
-  def self.create(attributes)
-    book = new(attributes)
-    book.save
+  def self.mark_all_cheaper_at_powells
+    all.each do |book|
+      book.cheaper_at_powells = book.locations.any? { |loc| loc['price'] && loc['price'].to_f < book.amazon_total_cost.to_f }
+      book.save
+    end
   end
 
   def initialize(attributes={})
@@ -24,7 +31,8 @@ class Book
      author: author,
      isbn: isbn,
      title: title,
-     locations: locations}
+     locations: locations,
+     cheaper_at_powells: cheaper_at_powells}
   end
 
   def locations
